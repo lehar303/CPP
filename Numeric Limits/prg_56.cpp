ed5ba6aa425da5e2e0cc310d8106700f79f1c8ed
Leharika <c.leharika-ext@alstomgroup.com>
2023-08-21 19:28:03 +0530

    21/8

---
+++ Numeric Limits/prg_56.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <limits>
+
+int main(){
+
+    std::cout << "Range for short is from " << std::numeric_limits<short>::min() << " to " << std::numeric_limits<short>::max() << std::endl;
+    std::cout << "Range for unsigned short is from " << std::numeric_limits<unsigned short>::min() << " to " << std::numeric_limits<unsigned short>::max() << std::endl;
+    std::cout << "Range for int is from " << std::numeric_limits<int>::min() << " to " << std::numeric_limits<int>::max() << std::endl;
+    std::cout << "Range for unsigned int is from " << std::numeric_limits<unsigned int>::min() << " to " << std::numeric_limits<unsigned int>::max() << std::endl;
+    std::cout << "Range for long is from " << std::numeric_limits<long>::min() << " to " << std::numeric_limits<long>::max() << std::endl;
+    std::cout << "Range for float is from " << std::numeric_limits<float>::min() << " to " << std::numeric_limits<float>::max() << std::endl;
+    //std::cout << "Range for float is from " << std::numeric_limits<float>::min() << " to " << std::numeric_limits<float>::max() << std::endl;
+
+
+    return 0;
+}
